a1e9919c922b9945ad065c24860a37a7c3e84106
mateusbsal4 <mateus.salomao4@usp.br>
2025-02-18 01:21:42 +0100

    Restructure code

--- scripts/IK_ompl.py
+++ scripts/IK_ompl.py
@@ -1,135 +1,100 @@
 import genesis as gs
 import numpy as np
 import torch
-########################## init ##########################
-gs.init(backend=gs.gpu)
 
-########################## create a scene ##########################
-scene = gs.Scene(
-    viewer_options=gs.options.ViewerOptions(
-        camera_pos=(3, -1, 1.5),
-        camera_lookat=(0.0, 0.0, 0.5),
-        camera_fov=30,
-        max_FPS=60,
-    ),
-    sim_options=gs.options.SimOptions(
-        dt=0.01,
-    ),
-    show_viewer=True,
-    show_FPS=False,
-)
+class IK_Controller:
+    def __init__(self):
+        # Genesis initialization
+        gs.init(backend=gs.gpu)
 
-########################## entities ##########################
-plane = scene.add_entity(
-    gs.morphs.Plane(),
-)
-scene.add_entity(
-    gs.morphs.Sphere(
-        radius=0.1, 
-        pos=(0.25, 0.3, 0.8),
-        fixed = True,
-    )
-)
-scene.add_entity(
-    gs.morphs.Sphere(
-        radius=0.1, 
-        pos=(0.25, -0.3, 0.8),
-        fixed = True,
-    )
-)   
-scene.add_entity(
-    gs.morphs.Sphere(
-        radius=0.1, 
-        pos=(0.5, 0.3, 0.6),
-        fixed = True,
-    )
-)
-scene.add_entity(
-    gs.morphs.Sphere(
-        radius=0.1, 
-        pos=(0.5, -0.3, 0.6),
-        fixed = True,
-    )
-)
-#scene.add_entity(
-#    gs.morphs.Sphere(
-#        radius=0.1, 
-#        pos=(0.65, 0, 0.3),
-#        fixed = True,
-#    )
-#)
-scene.add_entity(
-    gs.morphs.Sphere(
-        radius=0.05, 
-        pos=(0.25, -0.1, 0.3),
-        fixed = True,
-    )
-)
-franka = scene.add_entity(
-    gs.morphs.MJCF(file="xml/franka_emika_panda/panda.xml"),
-)
-########################## build ##########################
-scene.build()
-scene.draw_debug_sphere(
-    pos=(0.65, 0.0, 0.13),
-    radius=0.02,
-    color=(1, 0, 0),
-)
+        # Setup the environment
+        self.setup_environment()
 
-motors_dof = np.arange(7)
-fingers_dof = np.arange(7, 9)
+        # Build the scene
+        self.scene.build()
 
-# set control gains
-franka.set_dofs_kp(
-    np.array([4500, 4500, 3500, 3500, 2000, 2000, 2000, 100, 100]),
-)
-franka.set_dofs_kv(
-    np.array([450, 450, 350, 350, 200, 200, 200, 10, 10]),
-)
-franka.set_dofs_force_range(
-    np.array([-87, -87, -87, -87, -12, -12, -12, -100, -100]),
-    np.array([87, 87, 87, 87, 12, 12, 12, 100, 100]),
-)
+        # Set control gains
+        self.configure_controller()
 
-end_effector = franka.get_link("hand")
+        #Set goal EE position
+        self.goal_pos = [0.65, 0.0, 0.13]
+
+    def setup_environment(self):
+        # Create a scene
+        self.scene = gs.Scene(
+            viewer_options=gs.options.ViewerOptions(
+                camera_pos=(3, -1, 1.5),
+                camera_lookat=(0.0, 0.0, 0.5),
+                camera_fov=30,
+                max_FPS=60,
+            ),
+            sim_options=gs.options.SimOptions(
+                dt=0.01,
+            ),
+            show_viewer=True,
+            show_FPS=False,
+        )
+        self.scene.add_entity(gs.morphs.Plane())
+        self.scene.add_entity(gs.morphs.Sphere(radius=0.1, pos=(0.25, 0.3, 0.8), fixed=True))
+        self.scene.add_entity(gs.morphs.Sphere(radius=0.1, pos=(0.25, -0.3, 0.8), fixed=True))
+        self.scene.add_entity(gs.morphs.Sphere(radius=0.1, pos=(0.5, 0.3, 0.6), fixed=True))
+        self.scene.add_entity(gs.morphs.Sphere(radius=0.1, pos=(0.5, -0.3, 0.6), fixed=True))
+        self.scene.add_entity(gs.morphs.Sphere(radius=0.05, pos=(0.25, -0.1, 0.3), fixed=True))
+
+        self.franka = self.scene.add_entity(gs.morphs.MJCF(file="xml/franka_emika_panda/panda.xml"))
+
+    def configure_controller(self):
+        self.franka.set_dofs_kp(np.array([4500, 4500, 3500, 3500, 2000, 2000, 2000, 100, 100]))
+        self.franka.set_dofs_kv(np.array([450, 450, 350, 350, 200, 200, 200, 10, 10]))
+        self.franka.set_dofs_force_range(
+            np.array([-87, -87, -87, -87, -12, -12, -12, -100, -100]),
+            np.array([87, 87, 87, 87, 12, 12, 12, 100, 100])
+        )
+
+    def plan_path(self):
+        self.scene.draw_debug_sphere(pos=self.goal_pos, radius=0.02, color=(1, 0, 0))
+
+        self.end_effector = self.franka.get_link("hand")
+        self.prev_eepos = self.end_effector.get_pos()
+        if isinstance(self.prev_eepos, torch.Tensor):
+            self.prev_eepos = self.prev_eepos.cpu().numpy()
+
+        # Target joint angles 
+        qpos = self.franka.inverse_kinematics(
+            link=self.end_effector,
+            pos=np.array(self.goal_pos),
+            quat=np.array([0, 1, 0, 0]),
+        )
+        # Gripper open
+        qpos[-2:] = 0.04
+
+        self.path = self.franka.plan_path(
+            qpos_goal=qpos,
+            num_waypoints=200, # 2s duration
+        )         
 
 
-# reach the target pose 
-qpos = franka.inverse_kinematics(
-    link=end_effector,
-    pos=np.array([0.65, 0.0, 0.13]),
-    quat=np.array([0, 1, 0, 0]),
-)
-# gripper open
-qpos[-2:] = 0.04
+    def execute_path(self):
+        # Execute the planned path
+        for waypoint in self.path:
+            self.franka.control_dofs_position(waypoint)
 
-path = franka.plan_path(
-    qpos_goal     = qpos,
-    num_waypoints = 200, # 2s duration
-) 
+            ee_pos = self.end_effector.get_pos()    # Trajectory visualization
+            if isinstance(ee_pos, torch.Tensor):
+                ee_pos = ee_pos.cpu().numpy()
+            self.scene.draw_debug_line(
+                start=self.prev_eepos,
+                end=ee_pos,
+                color=(0, 1, 0),
+            )
+            self.prev_eepos = ee_pos
 
-prev_eepos = franka.get_link("hand").get_pos()
-if isinstance(prev_eepos, torch.Tensor):
-    prev_eepos = prev_eepos.cpu().numpy()
-print("Prev ee pos: ", prev_eepos)
-print("Type ee pos: ", type(prev_eepos))
+            self.scene.step()
 
-# execute the planned path
-for waypoint in path:
-    franka.control_dofs_position(waypoint)
+        while True:
+            self.scene.step()
 
-    ee_pos = franka.get_link("hand").get_pos()    # Trajectory visualization
-    if isinstance(ee_pos, torch.Tensor):
-        ee_pos = ee_pos.cpu().numpy()
-    scene.draw_debug_line(
-        start=prev_eepos,
-        end=ee_pos,
-        color=(0, 1, 0),
-    )
-    prev_eepos = ee_pos
-
-    scene.step()
-
-
-while(True):
-    scene.step()
+if __name__ == "__main__":
+    ik_controller = IK_Controller()
+    ik_controller.plan_path()
+    ik_controller.execute_path()
--- scripts/IK_pmaf.py
+++ scripts/IK_pmaf.py
@@ -4,145 +4,104 @@ import numpy as np
 import rospy
 from geometry_msgs.msg import Point
 
-target_pos = np.array([0.0, 0.0, 0.0])
-data_received = False
+class IK_Controller:
+    def __init__(self):
+        self.target_pos = np.array([0.0, 0.0, 0.0])
+        self.data_received = False
 
-def goal_pos_callback(data):
-    global target_pos, data_received
-    data_received = True
-    target_pos[0] = data.x
-    target_pos[1] = data.y
-    target_pos[2] = data.z
+        # ROS node initializations
+        rospy.init_node('ik_genesis_node', anonymous=True)
+        self.goal_pos_sub = rospy.Subscriber("agent_position", Point, self.goal_pos_callback)
+        self.rate = rospy.Rate(50)  
 
+        # Genesis initialization
+        gs.init(backend=gs.gpu)
 
+        # Setup the environment
+        self.setup_environment()
 
-def main():
-    # ROS node initializations
-    rospy.init_node('ik_genesis_node', anonymous=True)    
-    goal_pos_sub = rospy.Subscriber("agent_position", Point, goal_pos_callback)
-    rate = rospy.Rate(50)  # 10 Hz
-    
+        # Build the scene
+        self.scene.build()
 
-    ########################## Genesis init ##########################
-    gs.init(backend=gs.gpu)         
+        # Set control gains
+        self.configure_controller()
 
-    ########################## create a scene ##########################
-    scene = gs.Scene(
-        viewer_options=gs.options.ViewerOptions(
-            camera_pos=(3, -1, 1.5),
-            camera_lookat=(0.0, 0.0, 0.5),
-            camera_fov=30,
-            max_FPS=60,
-        ),
-        sim_options=gs.options.SimOptions(
-            dt=0.01,
-        ),
-        show_viewer=True,
-        show_FPS=False,
-    )
+    def goal_pos_callback(self, data):
+        self.data_received = True
+        self.target_pos[0] = data.x
+        self.target_pos[1] = data.y
+        self.target_pos[2] = data.z
 
-    ########################## entities ##########################
-    plane = scene.add_entity(
-        gs.morphs.Plane(),
-    )
-    scene.add_entity(
-        gs.morphs.Sphere(
-            radius=0.1, 
-            pos=(0.25, 0.3, 0.8),
-            fixed = True,
+    def setup_environment(self):
+        # Create a scene
+        self.scene = gs.Scene(
+            viewer_options=gs.options.ViewerOptions(
+                camera_pos=(3, -1, 1.5),
+                camera_lookat=(0.0, 0.0, 0.5),
+                camera_fov=30,
+                max_FPS=60,
+            ),
+            sim_options=gs.options.SimOptions(
+                dt=0.01,
+            ),
+            show_viewer=True,
+            show_FPS=False,
         )
-    )
-    scene.add_entity(
-        gs.morphs.Sphere(
-            radius=0.1, 
-            pos=(0.25, -0.3, 0.8),
-            fixed = True,
+        self.scene.add_entity(gs.morphs.Plane())
+        self.scene.add_entity(gs.morphs.Sphere(radius=0.1, pos=(0.25, 0.3, 0.8), fixed=True))
+        self.scene.add_entity(gs.morphs.Sphere(radius=0.1, pos=(0.25, -0.3, 0.8), fixed=True))
+        self.scene.add_entity(gs.morphs.Sphere(radius=0.1, pos=(0.5, 0.3, 0.6), fixed=True))
+        self.scene.add_entity(gs.morphs.Sphere(radius=0.1, pos=(0.5, -0.3, 0.6), fixed=True))
+        self.scene.add_entity(gs.morphs.Sphere(radius=0.05, pos=(0.25, -0.1, 0.3), fixed=True))
+
+        self.franka = self.scene.add_entity(gs.morphs.MJCF(file="xml/franka_emika_panda/panda.xml"))
+
+    def configure_controller(self):
+        self.franka.set_dofs_kp(np.array([4500, 4500, 3500, 3500, 2000, 2000, 2000, 100, 100]))
+        self.franka.set_dofs_kv(np.array([450, 450, 350, 350, 200, 200, 200, 10, 10]))
+        self.franka.set_dofs_force_range(
+            np.array([-87, -87, -87, -87, -12, -12, -12, -100, -100]),
+            np.array([87, 87, 87, 87, 12, 12, 12, 100, 100])
         )
-    )   
-    scene.add_entity(
-        gs.morphs.Sphere(
-            radius=0.1, 
-            pos=(0.5, 0.3, 0.6),
-            fixed = True,
+
+    def run(self):
+        self.scene.draw_debug_sphere(
+            pos=(0.65, 0.0, 0.13),
+            radius=0.02,
+            color=(1, 0, 0),
         )
-    )
-    scene.add_entity(
-        gs.morphs.Sphere(
-            radius=0.1, 
-            pos=(0.5, -0.3, 0.6),
-            fixed = True,
-        )
-    )
-    scene.add_entity(
-        gs.morphs.Sphere(
-            radius=0.05, 
-            pos=(0.25, -0.1, 0.3),
-            fixed = True,
-        )
-    )
 
-    franka = scene.add_entity(
-        gs.morphs.MJCF(file="xml/franka_emika_panda/panda.xml"),
-    )
-    ########################## build ##########################
-    scene.build()
+        self.end_effector = self.franka.get_link("hand")
+        self.prev_eepos = self.end_effector.get_pos()
+        if isinstance(self.prev_eepos, torch.Tensor):
+            self.prev_eepos = self.prev_eepos.cpu().numpy()
 
-    scene.draw_debug_sphere(
-        pos=(0.65, 0.0, 0.13),
-        radius=0.02,
-        color=(1, 0, 0),
-    )
+        while not rospy.is_shutdown():
+            if self.data_received:
+                qpos = self.franka.inverse_kinematics(
+                    link=self.end_effector,
+                    pos=self.target_pos,
+                    quat=np.array([0, 1, 0, 0]),
+                )
+                # Gripper open pos
+                qpos[-2:] = 0.04
+                self.franka.control_dofs_position(qpos[:-2], np.arange(7))
 
-    motors_dof = np.arange(7)
-    fingers_dof = np.arange(7, 9)
+                # Trajectory visualization
+                ee_pos = self.end_effector.get_pos()
+                if isinstance(ee_pos, torch.Tensor):
+                    ee_pos = ee_pos.cpu().numpy()
+                self.scene.draw_debug_line(
+                    start=self.prev_eepos,
+                    end=ee_pos,
+                    color=(0, 1, 0),
+                )
+                self.prev_eepos = ee_pos
 
-    # set control gains
-    franka.set_dofs_kp(
-        np.array([4500, 4500, 3500, 3500, 2000, 2000, 2000, 100, 100]),
-    )
-    franka.set_dofs_kv(
-        np.array([450, 450, 350, 350, 200, 200, 200, 10, 10]),
-    )
-    franka.set_dofs_force_range(
-        np.array([-87, -87, -87, -87, -12, -12, -12, -100, -100]),
-        np.array([87, 87, 87, 87, 12, 12, 12, 100, 100]),
-    )
+                self.scene.step()
 
-    end_effector = franka.get_link("hand")
-
-
-
-    prev_eepos = franka.get_link("hand").get_pos()
-    if isinstance(prev_eepos, torch.Tensor):
-        prev_eepos = prev_eepos.cpu().numpy()
-
-    while not rospy.is_shutdown():
-        if(data_received):
-            # move to pre-grasp pose
-            qpos = franka.inverse_kinematics(
-                link=end_effector,
-                pos=target_pos,
-                quat=np.array([0, 1, 0, 0]),
-            )
-            # gripper open pos
-            qpos[-2:] = 0.04
-            franka.control_dofs_position(qpos[:-2], motors_dof)
-
-            ##### Trajectory visualization ####
-            ee_pos = franka.get_link("hand").get_pos()    
-            if isinstance(ee_pos, torch.Tensor):
-                ee_pos = ee_pos.cpu().numpy()
-            scene.draw_debug_line(
-                start=prev_eepos,
-                end=ee_pos,
-                color=(0, 1, 0),
-            )
-            prev_eepos = ee_pos
-            ##################################
-
-            scene.step()
-
-        rate.sleep()
+            self.rate.sleep()
 
 if __name__ == "__main__":
-    main()
+    ik_controller = IK_Controller()
+    ik_controller.run()
